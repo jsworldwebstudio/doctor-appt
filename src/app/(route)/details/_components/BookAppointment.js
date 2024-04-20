f8e96688b8a68ad18ed0a9cd1e3734c17ba094ba
James Slaughter <jsworldwebstudio@gmail.com>
2024-04-20 16:45:53 -0500

    fix book appointment screen

--- src/app/(route)/details/_components/BookAppointment.js
+++ src/app/(route)/details/_components/BookAppointment.js
@@ -133,7 +133,7 @@ function BookAppointment({doctorId}) {
     return day<=new Date();
   }
   return (
-    <Dialog>
+    <Dialog className="grid grid-cols-1 md:grid-cols-2">
       <DialogTrigger>
         <Button className="mt-3 rounded-full">Book Appointment</Button>
       </DialogTrigger>
